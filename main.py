81c4e6c92d3af7433de0207e6dad2252d1ca91c9
alehey <alexey.p.svyz@mail.ru>
2023-03-15 20:22:32 +0700

    add main

---
+++ main.py
@@ -0,0 +1,66 @@
+# print('Введите первую строку')
+# a = int(input())
+# b = int(input('Введите второе число: '))
+
+# print (a, ' + ', b, ' = ', a+b)
+
+# a = 5.234325
+# b = 6.65523
+# print(round(a*b, 3))
+
+# iter = 2
+# iter +=3 iter = iter + 3
+
+# username = input('Input name: ')
+# if username == 'Masha':
+#     print('Ura, it is Masha!')
+# else:
+#     print('Hello, ', username)
+
+# n = int(input('Input number: '))
+# sum = 0
+# while n > 0:
+#     x = n % 10
+#     sum = sum + x
+#     n = n // 10
+# print(sum)
+
+# i = 0
+# while i < 5:
+#     # if i == 3:
+#     #     break
+#     i+=1
+# else:
+#     print('Stop')
+# print(i)
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0: # если остаток при делении числа n на i равен 0
+#         flag = False
+#         print(i)
+#     elif i > n // 2: # делить числа не может превышать введеное число, деленное на 2
+#         print(n)
+#         flag = False
+#     i+=1
+
+# r = range(100, 0, -20)
+# for i in r:
+#     print(i)
+
+# a = 'qwerty'
+
+# # print(a[2])
+# for i in a:
+#     print(i)
+
+text = 'Сеоеова цукоапо цукавп МИалаор'
+# #print(len(text))
+
+# # print('паупв' in text)
+# print(text.lower()) 
+# print(text[0]) 
+print(text[len(text)-1])
+print(text[-1])  
